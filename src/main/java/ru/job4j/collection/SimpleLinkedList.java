de3b2dfd8c7c5ebedf3a508aba567407fa827b94
opossum <ryabkoff@rambler.ru>
2022-06-16 17:10:11 +0500

    2. Создать контейнер на базе связанного списка [#159]

---
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -0,0 +1,70 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements LinkedList<E> {
+    private Node<E> head;
+    private Node<E> last;
+    private int size;
+    private int modCount;
+
+    @Override
+    public void add(E value) {
+        Node<E> node = new Node<>(value);
+        if (head == null) {
+            head = node;
+        } else {
+            last.next = node;
+        }
+        last = node;
+        modCount++;
+        size++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> point = head;
+        for (int i = 0; i < index; i++) {
+            point = point.next;
+        }
+        return point.value;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            Node<E> point;
+            private final int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return point != last;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                point = point == null ? head : point.next;
+                return point.value;
+            }
+        };
+    }
+
+    class Node<T> {
+        T value;
+        Node<T> next;
+
+        public Node(T value) {
+            this.value = value;
+        }
+    }
+}
