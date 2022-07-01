cbc58b0038ab28e41c304dcf7b05401f59ff0202
opossum <ryabkoff@rambler.ru>
2022-07-01 20:21:34 +0500

    2. Анализ доступности сервера. [#859]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -4,35 +4,18 @@ import java.io.*;
 import java.util.*;
 
 public class Analizy {
-    class Period {
-        String start;
-        String finish;
-
-        Period(String start, String finish) {
-            this.start = start;
-            this.finish = finish;
-        }
-    }
     public void unavailable(String source, String target) {
-        List<Period> periods = new ArrayList<>();
-        try (BufferedReader read = new BufferedReader(new FileReader(source))) {
-            boolean isOpen = false;
+        try (BufferedReader read = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+            String period = "";
             for (String line = read.readLine(); line != null; line = read.readLine()) {
                 String[] s = line.split(" ", 2);
-                if (!isOpen && ("400".equals(s[0]) || "500".equals(s[0]))) {
-                    periods.add(new Period(s[1], ""));
-                    isOpen = true;
-                } else if (isOpen && ("200".equals(s[0]) || "300".equals(s[0]))) {
-                    periods.get(periods.size() - 1).finish = s[1];
-                    isOpen = false;
+                if (period.isBlank() && ("400".equals(s[0]) || "500".equals(s[0]))) {
+                    period = s[1];
+                } else if (!period.isBlank() && ("200".equals(s[0]) || "300".equals(s[0]))) {
+                    out.println(period + ";" + s[1]);
+                    period = "";
                 }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-            for (Period period : periods) {
-                out.println(period.start + ";" + period.finish);
             }
         } catch (Exception e) {
             e.printStackTrace();
