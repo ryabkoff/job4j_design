3617222b815caea5aa5838f021ff2e1c80ea1b39
opossum <ryabkoff@rambler.ru>
2022-07-01 09:48:06 +0500

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/MultTable.java
@@ -0,0 +1,18 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class MultTable {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("mult.txt")) {
+            for (int x = 1; x <= 9; x++) {
+                for (int y = 1; y <= 9; y++) {
+                    out.write((x * y + "\t").getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
