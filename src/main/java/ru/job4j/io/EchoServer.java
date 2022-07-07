a173e4019c6b917c521ef4119a4dcbb22677e3b0
opossum <ryabkoff@rambler.ru>
2022-07-07 16:02:13 +0500

    2. Что такое Socket? [#4850]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -16,14 +16,11 @@ public class EchoServer {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
-                        if (str.contains("msg=Bye")) {
-                            closeServer = true;
+                        if (str.contains("/?msg=Bye")) {
+                            server.close();
                         }
                     }
                     out.flush();
-                }
-                if (closeServer) {
-                    server.close();
                 }
             }
         }
