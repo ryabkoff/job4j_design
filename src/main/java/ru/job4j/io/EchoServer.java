60cc321213325cb011e603e4007a4826e0423632
opossum <ryabkoff@rambler.ru>
2022-07-07 21:35:24 +0500

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,9 +3,13 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -13,23 +17,22 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    int startMsg;
-                    String msg = "";
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        startMsg = str.indexOf("/?msg=");
-                        if (startMsg != -1) {
-                            msg = str.substring(startMsg + 6, str.indexOf(" HTTP"));
-                            break;
+                        if (str.contains("?msg=Exit ")) {
+                            server.close();
+                        } else if (str.contains("?msg=Hello ")) {
+                            out.write("Hello\r\n".getBytes());
+                        } else if (str.contains("?msg=")) {
+                            out.write("What\r\n".getBytes());
                         }
                     }
-                    if ("Exit".equals(msg)) {
-                        server.close();
-                    } else if (!msg.isBlank()) {
-                        out.write((("Hello".equals(msg) ? "Hello" : "What") + "\r\n").getBytes());
-                    }
                     out.flush();
+                } catch (Exception e) {
+                    LOG.error("Exception", e);
                 }
             }
+        } catch (Exception e) {
+            LOG.error("Exception", e);
         }
     }
 }
