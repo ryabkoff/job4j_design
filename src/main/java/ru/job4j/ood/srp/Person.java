b638755782b64735d5554f5ac511caeb974420fd
opossum <ryabkoff@rambler.ru>
2022-08-02 22:20:28 +0500

    0. Принцип единственной ответственности [#4913]

---
+++ src/main/java/ru/job4j/ood/srp/Person.java
@@ -0,0 +1,57 @@
+package ru.job4j.ood.srp;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{"
+                + "name='" + name + '\''
+                + ", age=" + age
+                + '}';
+    }
+
+    /*
+    3. Нарушение принципа srp, метод выводит строку, что не относится к функциональности класса
+     */
+    public void printString(String value) {
+        System.out.println(value);
+    }
+}
