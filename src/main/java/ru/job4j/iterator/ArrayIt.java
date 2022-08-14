98c0ca0d494a2082dd104baec592ffed61900c70
opossum <ryabkoff@rambler.ru>
2022-08-14 18:29:16 +0500

    Подключение библиотеки AssertJ [#504881]

--- src/main/java/ru/job4j/iterator/ArrayIt.java
+++ src/main/java/ru/job4j/iterator/ArrayIt.java
@@ -1,7 +1,6 @@
 package ru.job4j.iterator;
 
 import java.util.Iterator;
-import java.util.NoSuchElementException;
 
 public class ArrayIt implements Iterator<Integer> {
     private final int[] data;
@@ -18,9 +17,6 @@ public class ArrayIt implements Iterator<Integer> {
 
     @Override
     public Integer next() {
-        if (!hasNext()) {
-            throw new NoSuchElementException();
-        }
         return data[point++];
     }
 }
--- src/test/java/ru/job4j/iterator/ArrayItTest.java
+++ src/test/java/ru/job4j/iterator/ArrayItTest.java
@@ -1,38 +1,26 @@
 package ru.job4j.iterator;
 
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.Matchers.is;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
-import org.junit.Test;
-
-import java.util.NoSuchElementException;
-
-public class ArrayItTest {
-
+class ArrayItTest {
     @Test
-    public void whenMultiCallhasNextThenTrue() {
+    void whenMultiCallHasNextThenTrue() {
         ArrayIt it = new ArrayIt(
                 new int[] {1, 2, 3}
         );
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.hasNext(), is(true));
+        boolean rsl = it.hasNext();
+        assertThat(rsl).isTrue();
+        assertThat(it.hasNext()).isTrue();
     }
 
     @Test
-    public void whenReadSequence() {
+    void whenReadSequence() {
         ArrayIt it = new ArrayIt(
                 new int[] {1, 2, 3}
         );
-        assertThat(it.next(), is(1));
-        assertThat(it.next(), is(2));
-        assertThat(it.next(), is(3));
-    }
-
-    @Test(expected = NoSuchElementException.class)
-    public void whenNextFromEmpty() {
-        ArrayIt it = new ArrayIt(
-                new int[]{}
-        );
-        it.next();
+        assertThat(it.next()).isEqualTo(1);
+        assertThat(it.next()).isEqualTo(2);
+        assertThat(it.next()).isEqualTo(3);
     }
 }
